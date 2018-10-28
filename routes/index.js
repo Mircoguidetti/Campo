31888b329fd9b7d8110a6efcb4acfd88f3c7a8e3
github username <github email address>
2018-10-28 14:35:01 +0000

    Setup flash messages

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-const User  = require("../models/user");
+const User  = require("../models/users");
 
 //root route
 router.get("/", (req, res) => {
