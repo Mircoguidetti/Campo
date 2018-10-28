d6bb38817c055656eb80f7a96d3ffe8935cadb26
github username <github email address>
2018-10-28 14:48:52 +0000

    Style home page

--- app.js
+++ app.js
@@ -58,5 +58,5 @@ app.use(indexRoutes);
 app.listen(process.env.PORT || 3000, () => {
   console.log(process.env.DATABASEURL)
   console.log(process.env.PORT)
-  console.log("The YelpCamp Server Has Started");
+  console.log("The Campo Server Has Started");
 });
