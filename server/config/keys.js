d24a0f4bf6b560d88dbf7af2a8a523a6d0b85f3f
github username <github email address>
2018-11-23 12:31:32 +0000

    Add google map script in show.ejs file

--- server/config/keys.js
+++ server/config/keys.js
@@ -3,6 +3,7 @@ module.exports = {
     cloudinaryName: 'djwkv5jke',
     cloudinaryApiKey: 614712633294651,
     cloudinaryApiSecret: 'flhK0d0ZtOCE7cjBeaKR69LEYZs',
+    googleMapsKey: 'AIzaSyARO1OZv5iL7LZQPHP1ZqWoRylH6ewEyps',
     googleClientID: '720893765345-q0nckdkj1lukq5v4lb5om1e9muk6ekaq.apps.googleusercontent.com',
     googleClientSecret: 'VlHavaMBlcoWlT5EITX2ZCbd'
 };
